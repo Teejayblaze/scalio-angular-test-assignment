dee85fd450b1760e702e6a5251cc99ac50c8bbb8
Teejayblaze <surfsamson@gmail.com>
2021-06-19 18:38:17 +0100

    1 file modified, 4 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { SearchComponent } from './components/search/search.component';
+import { ResultsComponent } from './components/results/results.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent, HelloComponent, SearchComponent, ResultsComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
