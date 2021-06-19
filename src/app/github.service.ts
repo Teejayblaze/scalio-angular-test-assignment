aab566ad24b2307b78f68b33c12b98b151c25721
Teejayblaze <surfsamson@gmail.com>
2021-06-19 20:04:21 +0100

    1 file modified

--- src/app/github.service.ts
+++ src/app/github.service.ts
@@ -1,6 +1,5 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Users } from './models/users';
 
 @Injectable()
 export class GithubService {
