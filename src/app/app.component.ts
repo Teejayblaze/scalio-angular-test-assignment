c8d193fe9b06c02f1f3165fab92db271519d1da2
Teejayblaze <surfsamson@gmail.com>
2021-06-19 19:17:10 +0100

    4 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,16 @@ import { Component, VERSION } from '@angular/core';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent  {
+export class AppComponent {
+  key: string = 'avatar_url';
+  reverse: boolean = false;
+
   name = 'Angular ' + VERSION.major;
+
+  sort(key: string = null) {
+    this.key = key;
+    this.reverse = !this.reverse;
+  }
 }
